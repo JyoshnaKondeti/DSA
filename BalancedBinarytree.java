40ccd514385fa715f807e53f6e63ef2f1e28d11d
Jyoshna Kondeti <jyoshnakondeti14@gmail.com>
2025-07-15 21:39:34 -0500

    balancedbinarytree

---
+++ BalancedBinarytree.java
@@ -0,0 +1,15 @@
+class BalancedBinarytree {
+    public boolean isBalanced(TreeNode root) {
+        return checkheight(root)!=1;
+    }
+    public int checkheight(TreeNode root){
+        if(root == null) return 0;
+        int left = checkheight(root.left);
+        if (left ==-1) return -1;
+        int right = checkheight(root.right);
+        if (right ==-1) return -1;
+        if (Math.abs(left-right)>1) return -1;
+        return Math.max(left,right) +1;
+
+    }
+}
