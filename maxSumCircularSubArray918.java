b8376d26781d122286b49b5ba7057c636c2d4115
Jyoshna Kondeti <jyoshnakondeti14@gmail.com>
2025-09-05 15:55:48 -0500

    9/5

---
+++ maxSumCircularSubArray918.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int totalSum=0;
+        int maxSum=nums[0];
+        int minSum=nums[0];
+        int currMax=0;
+        int currMin=0;
+        for(int num:nums){
+            currMax = Math.max(currMax+num, num);
+            maxSum = Math.max(currMax, maxSum);
+
+            currMin= Math.min(currMin+num, num);
+            minSum= Math.min(currMin, minSum);
+
+            totalSum+=num;
+        }
+        if(maxSum<0) return maxSum;
+        return Math.max(maxSum, totalSum-minSum);
+    }
+}
