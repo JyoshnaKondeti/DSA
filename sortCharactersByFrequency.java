91eca2ccf463bf84fbce481cdd70250b96216a2b
Jyoshna Kondeti <jyoshnakondeti14@gmail.com>
2025-10-22 13:57:06 -0500

    frequencySorting

---
+++ sortCharactersByFrequency.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(char i: s.toCharArray()){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+
+        List<Character>[] buckets = new List[s.length() +1];
+        for(char c: map.keySet()){
+            int freq = map.get(c);
+            if(buckets[freq] == null){
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(c);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(int i=buckets.length-1; i>=0; i--){
+            if(buckets[i] != null){
+                for(char c: buckets[i]){
+                    for(int j=0; j<i; j++){
+                        result.append(c);
+                    }
+                }
+            }
+        }
+        return result.toString();
+    }
+}
